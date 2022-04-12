5d3340c2dd703b6552b19323b807534077beae45
leighDEV <leighest2021@gmail.com>
2022-04-12 17:37:58 +0800

    Add files

---
+++ gpa-calculator-winforms/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace gpa_calculator_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbGrades = new System.Windows.Forms.ListBox();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.tbGrade = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbGrades
+            // 
+            this.lstbGrades.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbGrades.FormattingEnabled = true;
+            this.lstbGrades.ItemHeight = 25;
+            this.lstbGrades.Location = new System.Drawing.Point(273, 22);
+            this.lstbGrades.Name = "lstbGrades";
+            this.lstbGrades.Size = new System.Drawing.Size(126, 254);
+            this.lstbGrades.TabIndex = 1;
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade.Location = new System.Drawing.Point(26, 30);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(65, 25);
+            this.lblGrade.TabIndex = 0;
+            this.lblGrade.Text = "Grade:";
+            // 
+            // tbGrade
+            // 
+            this.tbGrade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbGrade.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbGrade.Location = new System.Drawing.Point(99, 22);
+            this.tbGrade.Name = "tbGrade";
+            this.tbGrade.Size = new System.Drawing.Size(158, 33);
+            this.tbGrade.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(99, 61);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(158, 34);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(99, 242);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(158, 34);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(99, 139);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(158, 34);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearAll.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearAll.Location = new System.Drawing.Point(99, 179);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(158, 34);
+            this.btnClearAll.TabIndex = 3;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.AutoSize = true;
+            this.lblGPA.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGPA.Location = new System.Drawing.Point(279, 293);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(55, 25);
+            this.lblGPA.TabIndex = 0;
+            this.lblGPA.Text = "GPA: ";
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(99, 99);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(158, 34);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(440, 340);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbGrade);
+            this.Controls.Add(this.lstbGrades);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.lblGrade);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GPA Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstbGrades;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.TextBox tbGrade;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Label lblGPA;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ gpa-calculator-winforms/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gpa_calculator_winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstbGrades.Items.Add(tbGrade.Text);
+            lblGPA.Text = "GPA: ";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbGrade.ResetText();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            lstbGrades.Items.Remove(lstbGrades.SelectedItem);
+            lblGPA.Text = "GPA: ";
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            lstbGrades.Items.Clear();
+            tbGrade.ResetText();
+            lblGPA.Text = "GPA: ";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double avg = 0, sum = 0;
+            int lstCount = lstbGrades.Items.Count; //Get Count of Listbox Items
+            for (int i = 0; i < lstCount; i++)
+            {
+                sum += Convert.ToDouble(lstbGrades.Items[i]);
+            }
+            avg = sum / lstCount;
+            Double gpa = Math.Round((Double)avg, 2);
+            lblGPA.Text = "GPA: " + gpa;
+        }
+
+    }
+}
